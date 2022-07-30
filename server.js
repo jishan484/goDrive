3c318dbcfd368b05c4bed3ec95e0aea0d8c7fe41
jishan484@gmail.com <jishan484@gmail.com>
2022-07-30 14:40:26 +0530

    fhgh

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const userService = require('./service/userService')
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT | 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-    res.set('Cache-Control', 'public, max-age=31536000');
+    if(req.url.search('portal') == -1){
+        res.set('Cache-Control', 'private, max-age=86400, stale-while-revalidate=604800');
+    }
     res.header("X-Powered-By", "MiFi-Server");
     next();
 });
