7a7dda95e89dab0625dac0b8c78bae1fd57b9b9c
jishan484@gmail.com <jishan484@gmail.com>
2022-07-30 19:00:30 +0530

    api renamed ! wtf i m doing. omg

---
+++ controller/app/auth.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const userService = require('../../service/userService');
+const { RouterConfig } = require('../../SystemConfig');
+const router = express.Router({ mergeParams: true });
+
+
+router.post('/login', userLogin);
+router.use('/logout', userLogout);
+router.post('/register', userRegister);
+
+module.exports = router;
+
+
+// MiddleWares:
+
+function userLogin(req, res) {
+    if (userService.isLoggedIn(req)) {
+        res.redirect(RouterConfig.home_page_uri);
+    }
+    else {
+        userService.userLogin(req, res).then((status) => {
+            if (status) {
+                res.status(200).send({ status: 'success', error: null, code: '200' });
+            }
+            else {
+                res.status(200).send({ status: 'failed', error: "Username and Password not matched", code: '204' });
+            }
+        });
+    }
+}
+
+function userLogout(req, res) {
+    userService.userLogout(res);
+    res.redirect(RouterConfig.force_login_redirect_uris);
+}
+
+
+function userRegister(req, res) {
+    res.send("ok");
+}
+
+function userValidationMiddleware(req, res, next) {
+    if (this.isLoggedIn(req)) {
+        next();
+    }
+    else {
+        res.redirect(RouterConfig.force_login_redirect_uris);
+    }
+}
