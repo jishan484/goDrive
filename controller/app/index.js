7326b4f8e3d13acb90337f1ee615208e7e9bdc58
jishan484@gmail.com <jishan484@gmail.com>
2022-08-16 01:35:24 +0530

    hu

--- controller/app/index.js
+++ controller/app/index.js
@@ -18,15 +18,13 @@ router.use("/u/*", (req, res, next) => {
         }
     }
     else {
-        req.on('data',(data)=>{
-            req.socket.end();
-        });
         res.status(403).send({
             uri: RouterConfig.force_login_redirect_uris,
             error: "You are not logged in",
             code: "403",
             data: null
         });
+        res.socket.destroy();
     }
 });
 
