98ba182cae9c4c71a03d0b718c5a28fbd85e0653
mifiadmin <admin@mifi.helioho.st>
2023-01-05 22:58:17 +0530

    push conflict fix

--- resources/public/js/dashboards-analytics.js
+++ resources/public/js/dashboards-analytics.js
@@ -1,6 +1,23 @@
 /**
  * Dashboard Analytics
  */
+let config = {
+  colors: {
+    primary: '#696cff',
+    secondary: '#8592a3',
+    success: '#71dd37',
+    info: '#03c3ec',
+    warning: '#ffab00',
+    danger: '#ff3e1d',
+    dark: '#233446',
+    black: '#000',
+    white: '#fff',
+    body: '#f4f5fb',
+    headingColor: '#566a7f',
+    axisColor: '#a1acb8',
+    borderColor: '#eceef1'
+  }
+};
 
 var chart = null, global_storage_status = {};
 global_storage_status.totalSize = 0;
