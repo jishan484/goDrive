3af0013088e2dccfc1f71605d1b47a52f8e1c389
mifiadmin <admin@mifi.helioho.st>
2023-01-05 22:41:52 +0530

    Some code cleanup

--- resources/public/js/dashboards-analytics.js
+++ resources/public/js/dashboards-analytics.js
@@ -4,6 +4,26 @@
 
 'use strict';
 
+// JS global variables
+let config = {
+  colors: {
+    primary: '#696cff',
+    secondary: '#8592a3',
+    success: '#71dd37',
+    info: '#03c3ec',
+    warning: '#ffab00',
+    danger: '#ff3e1d',
+    dark: '#233446',
+    black: '#000',
+    white: '#fff',
+    body: '#f4f5fb',
+    headingColor: '#566a7f',
+    axisColor: '#a1acb8',
+    borderColor: '#eceef1'
+  }
+};
+
+
 (function () {
   let cardColor, headingColor, axisColor, shadeColor, borderColor;
 
