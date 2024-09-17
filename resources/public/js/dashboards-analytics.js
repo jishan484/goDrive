1c66b4805d6421f96a93e1417e04ff4c9047e337
jishan484 <jishan484@gmail.com>
2024-09-18 01:59:52 +0530

    UI bug fixes - user home page

--- resources/public/js/dashboards-analytics.js
+++ resources/public/js/dashboards-analytics.js
@@ -114,8 +114,6 @@ function renderStorageStatus(){
   });
 }
 
-setTimeout(renderStorageStatus, 2000);
-
 function generateRandomHexColor(){
   return '#'+Math.floor(Math.random()*16777215).toString(16);
 }
