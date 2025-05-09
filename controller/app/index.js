f14a74717f524229194f438be6be72d74169bf4a
jishan <49402826+jishan484@users.noreply.github.com>
2025-05-10 04:00:59 +0530

    File request option (#32)
    * file request option added (initial)
    
    * added wiki page
    
    * api docs and request page
    
    * fileRequestUI file info added

--- controller/app/index.js
+++ controller/app/index.js
@@ -52,7 +52,7 @@ function checkAuth(req,res,next){
             res.set("connection", "close");
             res.status(403).send({
                 uri: RouterConfig.force_login_redirect_urn,
-                error: "You are not logged in!",
+                error: "You are not allowed to call APIs outside UI App!",
                 code: "403",
                 data: null
             }).end();
